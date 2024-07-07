d61f45095825ba849c27440b294284fb1839a891
creddy9 <creddy9@ford.com>
2024-07-07 12:22:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Heart from 'react-animated-heart';
 
 function App() {
+  const [isClick, setClick] = useState(false);
   return (
     <div className="App">
-      <h1 color='green'> Welcome to the CHANDU<Heart isClick={isClick} onClick={() => setClick(!isClick)} />MOUNI to GitHubPages</h1>
+      <h1 color='green'> Welcome to the CHANDU <Heart className="heart-align" isClick={isClick} onClick={() => setClick(!isClick)} /> MOUNI to GitHubPages</h1>
     </div>
   );
 }
