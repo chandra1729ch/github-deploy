6019de2e16058742740a27a7f934b785899e443d
creddy9 <creddy9@ford.com>
2024-07-07 12:12:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 color='green'> Welcome to the CHANDU-MOUNI to GitHubPages</h1>
+      <h1 color='green'> Welcome to the CHANDU<Heart isClick={isClick} onClick={() => setClick(!isClick)} />MOUNI to GitHubPages</h1>
     </div>
   );
 }
