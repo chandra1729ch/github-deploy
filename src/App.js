91626b175ad225a8f59fea433663b9fc4d500e13
creddy9 <creddy9@ford.com>
2024-07-07 12:05:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 color='green'> Welcome to the CHANDU-MOUNI to GitHubPages</h1>
     </div>
   );
 }
